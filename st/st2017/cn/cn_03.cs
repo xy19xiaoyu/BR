cf25df7dee0a8b754e39717a0dce705636b5a787
xy19xiaoyu <0714010877@163.com>
2017-08-30 21:59:06 +0800

    x

--- st/st2017/cn/cn_03.cs
+++ st/st2017/cn/cn_03.cs
@@ -23,11 +23,21 @@ namespace ST_2017.cn
             Console.WriteLine("正在生成表：{0}\t{1}\t{2}\t{3}", ztName, this.config.FileName, this.Name, DateTime.Now.ToString());
             string sql = @"
 select 
-	an 申请号,
-	type 专利类型,
-	ad 申请日,
-	country 国家 
-from cn
+	cn.an 申请号,
+	cn.type 专利类型,
+	cn.ad 申请日,
+	country 来源国家,
+    cn_biblio.ti as 标题,
+    cn_biblio.abs as 摘要,
+    cn_pa.pa as 申请人
+from 
+    cn,
+    cn_biblio,
+    cn_pa
+where 
+    cn.sn = cn_biblio.sn
+    and cn.an = cn_pa.an
+    and cn_pa.sort = 0
 order by country";
 
             dt = DBA.SqlDbAccess.GetDataTable(CommandType.Text, sql);
